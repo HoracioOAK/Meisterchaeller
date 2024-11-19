9610b1eea5e042ff4770a95ea3135c2ca4c8d15e
HoracioOAK <hoa.keller@gmail.com>
2024-11-19 10:56:23 +0100

    made changes to Plot_multiples.py

--- utils/Plot_multiples.py
+++ utils/Plot_multiples.py
@@ -89,9 +89,10 @@ def plot_results(base_path, selected_dirs, desired_target=140):
 
         x = list(range(1, len(average_array) + 1))
         plt.plot(x, average_array, linestyle='-', label=f"{dir_name}, Runs = {number_of_runs}")
-        plt.fill_between(x,average_array, percentile_10, alpha=0.2, label=f"{dir_name} 10th Percentile")
-        plt.fill_between(x,average_array, percentile_90, alpha=0.2, label=f"{dir_name} 90th Percentile")
+        #plt.fill_between(x,average_array, percentile_10, alpha=0.2, label=f"{dir_name} 10th Percentile")
+        #plt.fill_between(x,average_array, percentile_90, alpha=0.2, label=f"{dir_name} 90th Percentile")
         #plt.fill_between(x,average_array - standard_deviation, average_array + standard_deviation, alpha=0.2, label=f"{dir_name} Std Dev")
+        plt.fill_between(x,percentile_10, percentile_90, alpha=0.2, label=f"{dir_name} 10  & 90 Percentile")
         plt.errorbar(x, average_array, yerr=standard_deviation, fmt='-.', label=f"{dir_name} Mean ± Std Dev")
     # Plotting settings
     plt.axhline(y=desired_target, color='r', linestyle='--', label='Desired Target')
